772fa9436ac6f46b2a3e3494acbfd15dfa15ef11
Samedusa4real <118839504+Samedusa4real@users.noreply.github.com>
2022-12-07 17:41:51 +0400

    Add files via upload

---
+++ task1cs/bolunme.cs
@@ -0,0 +1,12 @@
+﻿int a;
+
+Console.WriteLine("Eded daxil edin:");
+a = Convert.ToInt32(Console.ReadLine());
+
+    if (a%3==0 && a%7==0)
+    {
+        Console.WriteLine("Eded 3e ve 7ye qaliqsiz bolunur.");
+    } else
+    {
+        Console.WriteLine("Eded 3e ve 7ye qaliqli bolunur.");
+    }
---
+++ task1cs/qiymet.cs
@@ -0,0 +1,36 @@
+﻿int qiymet;
+Console.WriteLine("Imtahan qiymetini daxil edin:");
+
+qiymet = Convert.ToInt32(Console.ReadLine());
+
+
+if (qiymet >= 0 && qiymet <= 100)
+{
+    if (qiymet >= 91)
+    {
+        Console.WriteLine("A");
+    }
+    else if (qiymet >= 81)
+    {
+        Console.WriteLine("B");
+    }
+    else if (qiymet >= 71)
+    {
+        Console.WriteLine("C");
+    }
+    else if (qiymet >= 61)
+    {
+        Console.WriteLine("D");
+    }
+    else if (qiymet >= 51)
+    {
+        Console.WriteLine("E");
+    }
+    else
+    {
+        Console.WriteLine("F");
+    }
+} else
+{
+    Console.WriteLine("Duzgun qiymet daxil edin.");
+}
